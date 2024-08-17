d1a99a9d97b525fa9ad62628b3d28c85a50b643c
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-18 00:30:59 +0530

    Update board with user's choice and grid number

---
+++ lib/tictactoe/game.rb
@@ -0,0 +1,55 @@
+class Game
+  def initialize
+    @board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+  end
+
+  def display_dashes_row(game_board, arr)
+    arr.length.times do |n|
+      if n == arr.length - 1
+        game_board.concat('---')
+      else
+        game_board.concat('---|')
+      end
+    end
+    game_board.concat("\n")
+  end
+
+  def display_chars(game_board, elem)
+    if elem.instance_of? Integer
+      game_board.concat('   ')
+    else
+      game_board.concat(" #{elem} ")
+    end
+  end
+
+  def display_numbers_row(game_board, arr)
+    arr.each_with_index do |elem, idx2|
+      display_chars(game_board, elem)
+      game_board.concat('|') unless idx2 == arr.length - 1
+    end
+    game_board.concat("\n")
+  end
+
+  def display_board
+    game_board = ''
+    @board.each_with_index do |arr, idx1|
+      display_numbers_row(game_board, arr)
+      break if idx1 == @board.length - 1
+
+      display_dashes_row(game_board, arr)
+    end
+    game_board
+  end
+
+  def update_board(char_choice, grid_choice)
+    @board.each_with_index do |arr, idx1|
+      arr.each_with_index do |elem, idx2|
+        arr[idx2] = char_choice if elem == grid_choice
+      end
+    end
+  end
+end
