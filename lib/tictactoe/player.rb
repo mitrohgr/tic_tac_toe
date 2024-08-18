3658f1ae409eee7757c3340db8fea7056d5ab85a
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-18 17:41:13 +0530

    Modify method in Player class to control behavior

--- lib/tictactoe/player.rb
+++ lib/tictactoe/player.rb
@@ -1,8 +1,5 @@
 class Player
-  attr_reader :char_choice, :grid_choice
-
-  def initialize(char_choice, grid_choice)
-    @char_choice = char_choice
-    @grid_choice = grid_choice
+  def initialize
+    @choice = %w[X O].sample
   end
 end
