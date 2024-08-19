c3d7d1e6e487dff343f0490c786646f7e55833f4
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-19 13:27:25 +0530

    Add an instance variable to track the game status

--- lib/tictactoe.rb
+++ lib/tictactoe.rb
@@ -4,16 +4,25 @@ require_relative 'tictactoe/player'
 class TicTacToe
   def initialize
     @game = Game.new
+    @status = nil
     @player = Player.new
   end
 
   def start_game
     puts @game.display_board
-    until @game.over?
+    loop do
       print 'Enter grid number: '
       grid = gets.chomp.to_i
       @game.update_board(@player.choice, grid)
       puts @game.display_board
+      if @game.won?
+        @status = 'won'
+        break
+      elsif @game.drawn?
+        @status = 'drawn'
+        break
+      end
+
       @player.change_choice
     end
   end
